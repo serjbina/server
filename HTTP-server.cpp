68f7a2b005dedc8e0665ef6eebee7b33bc500b9f
serjbina <serjbina@mail.com>
2016-05-03 23:27:38 +0300

    Функции когда ответа вынесены из основной программы

--- HTTP-server.cpp
+++ HTTP-server.cpp
@@ -4,26 +4,11 @@
 #include <string>
 #include <time.h>
 
-#include "service_classes.cpp"
+#include "response_code.h" //Заголовочные файлы функций, определяющих код ответа
+#include "service_classes.cpp" //Содержит описание служебных классов
+
 
 using namespace std;
-
-
-
-char * find_name(char * buff); //Обнаруживает имя запрашиваемого файла в буфере данных
-char * define_type(char * name); //Определяет тип запрошенного файла по его имени
-int if_exist(char *name); //Возвращает 1 в случае наличия файла в директории, 0 - иначе
-int if_availible(char *name); //Возвращает 1 в случае наличия прав доступа к файлу, 0 - иначе
-
-int if_exist(char *name){
-	int marker = open(name, O_RDONLY);
-	if (marker == -1)
-		return 0;
-	else{
-		close(marker);
-		return 1;
-	}
-}
 
 
 void Nowatime(stringstream resp){
@@ -200,51 +185,3 @@ printf("End\n");
 	return 0;
 }
 
-char* find_name(char * buff)
-{
-		char *name = (char*) malloc (80*sizeof(char));
-		char *p = name;
-		while(*buff != '/')
-			buff++;
-		buff++;
-
-		while(*buff == ' ')
-			buff++;
-
-		while(*buff != ' '){//} and *str != '/'){
-			*p = *buff;
-			p++;
-			buff++;
-		}
-
-		*p = '\0';
-
-		if (!strcmp(name, "HTTP/1.1\r\nHost:"))
-			name = "index.html";
-
-		return name;
-
-}
-
-char *define_type(char *name){
-	char *p = name;
-	while (*p != '.')
-		p++;
-	p++;
-	char buff[50];
-	char *a = buff;
-	while(*p != '\0'){
-		*a = *p;
-		a++;
-		p++;
-	}
-	*a = '\0';
-	char *type = (char*) malloc (12*sizeof(char));
-	if(!strcmp(buff, "html"))
-		type = "text/html";
-	else if(!strcmp(buff, "jpeg") or (!strcmp(buff, "jpg")))
-		type = "image/jpeg";		
-	else
-		type = "text/plain";
-	return type;
-}
---
+++ response_code.cpp
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+
+char* find_name(char * buff)
+{
+		char *name = (char*) malloc (80*sizeof(char));
+		char *p = name;
+		while(*buff != '/')
+			buff++;
+		buff++;
+
+		while(*buff == ' ')
+			buff++;
+
+		while(*buff != ' '){
+			*p = *buff;
+			p++;
+			buff++;
+		}
+
+		*p = '\0';
+
+		if (!strcmp(name, "HTTP/1.1\r\nHost:"))
+			name = "index.html";
+
+		return name;
+
+}
+
+char *define_type(char *name){
+	char *p = name;
+	while (*p != '.')
+		p++;
+	p++;
+	char buff[50];
+	char *a = buff;
+	while(*p != '\0'){
+		*a = *p;
+		a++;
+		p++;
+	}
+	*a = '\0';
+	char *type = (char*) malloc (12*sizeof(char));
+	if(!strcmp(buff, "html"))
+		type = "text/html";
+	else if(!strcmp(buff, "jpeg") or (!strcmp(buff, "jpg")))
+		type = "image/jpeg";		
+	else
+		type = "text/plain";
+	return type;
+}
+
+int if_exist(char *name){
+	int marker = open(name, O_RDONLY);
+	if (marker == -1)
+		return 0;
+	else{
+		close(marker);
+		return 1;
+	}
+}
